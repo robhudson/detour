829b92ace88813464859fb12c2513ef38d1532cf
Rob Hudson <rob@cogit8.org>
2013-02-16 15:11:55 -0800

    Added start of API
    Also, moved a bunch of stuff around to handle testing better.

--- detour/app.py
+++ detour/app.py
@@ -1,80 +1,80 @@
 # -*- coding: utf-8 -*-
+import os
+
 import browserid
 from flask import Flask, g, jsonify, render_template, request, session
 from sqlalchemy.orm.exc import NoResultFound
 
 import settings
+from api import api
 from database import db
-from models import Message, User
+from models import *
 
 
-app = Flask(__name__)
-app.debug = getattr(settings, 'DEBUG', False)
-app.secret_key = settings.SESSION_SECRET
-# Database config is in database.py
+default_config = {
+    'DEBUG': getattr(settings, 'DEBUG', False),
+    'SECRET_KEY': settings.SESSION_SECRET,
+}
 
 
-@app.before_request
-def load_current_user():
-    g.user = (User.query.filter_by(email=session['email']).first()
-              if 'email' in session else None)
+def create_app(config):
+    app = Flask(__name__)
+    app.config.update(default_config)
+    app.config.update(config)
+    app.register_blueprint(api, url_prefix='/1.0')
 
+    db.app = app
+    db.init_app(app)
 
-@app.route('/', methods=['GET'])
-def main():
-    """Default landing page."""
-    return render_template('index.html', authenticated=bool(g.user))
+    @app.before_request
+    def load_current_user():
+        g.user = (User.query.filter_by(email=session['email']).first()
+                  if 'email' in session else None)
 
+    @app.route('/', methods=['GET'])
+    def main():
+        """Default landing page."""
+        return render_template('index.html', authenticated=bool(g.user))
 
-@app.route('/landing', methods=['GET'])
-def landing():
-    if g.user:
-        messages = Message.query.filter(
-            Message.to_user==g.user).all()
-        return render_template('_dashboard.html', messages=messages)
-    else:
-        return render_template('_landing.html')
+    @app.route('/authenticate', methods=['POST'])
+    def set_email():
+        """Verify via Persona.
 
+        Upon success, create the user if it doesn't already exist and set the email
+        for the user's session.
+        """
+        data = browserid.verify(request.form['assertion'], settings.SITE_URL)
+        email = data['email']
 
-@app.route('/authenticate', methods=['POST'])
-def set_email():
-    """Verify via Persona.
+        # Create user record.
+        try:
+            user = User.query.filter(User.email==email).one()
+        except NoResultFound:
+            user = User(email=email)
+            db.add(user)
+            db.commit()
 
-    Upon success, create the user if it doesn't already exist and set the email
-    for the user's session.
-    """
-    data = browserid.verify(request.form['assertion'], settings.SITE_URL)
-    email = data['email']
+        session['email'] = user.email
+        return jsonify({'message':'okay'})
 
-    # Create user record.
-    try:
-        user = User.query.filter(User.email==email).one()
-    except NoResultFound:
-        user = User(email=email)
-        db.add(user)
-        db.commit()
+    @app.route('/logout', methods=['GET', 'POST'])
+    def logout():
+        """Log the user out."""
+        session.pop('email', None)
+        return jsonify({'message':'okay'})
 
-    session['email'] = user.email
-    return jsonify({'message':'okay'})
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('404.html')
 
+    @app.errorhandler(500)
+    def something_broke(error):
+        return render_template('500.html')
 
-@app.route('/logout', methods=['GET', 'POST'])
-def logout():
-    """Log the user out."""
-    session.pop('email', None)
-    return jsonify({'message':'okay'})
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('404.html')
-
-
-@app.errorhandler(500)
-def something_broke(error):
-    return render_template('500.html')
-
+    return app
 
 if __name__ == '__main__':
-    app.debug = settings.DEBUG
+    app = create_app({'DEBUG': True,
+                      'SQLALCHEMY_DATABASE_URI': os.environ.get(
+                          'DATABASE_URL', 'sqlite:///detour_app.db')})
     app.run()
